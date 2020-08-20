d6e5cb8d0c720c43fbbbeeafb8a28458c4b1b924
Dmitry Rakovets <dmitryrakovets@gmail.com>
2020-08-20 08:00:03 +0300

    feat(example): add example for theme 'Apache Maven: Intro'

---
+++ apache-maven/intro/src/test/java/com/rakovets/course/java/tools/apache/maven/intro/example/FileUtilTest.java
@@ -0,0 +1,13 @@
+package com.rakovets.course.java.tools.apache.maven.intro.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FileUtilTest {
+    @Test
+    public void testGetFileExtension() {
+        FileUtil fileUtil = new FileUtil();
+        Assertions.assertEquals("", fileUtil.getFileExtension(""));
+        Assertions.assertEquals("sh", fileUtil.getFileExtension("name.sh"));
+    }
+}
