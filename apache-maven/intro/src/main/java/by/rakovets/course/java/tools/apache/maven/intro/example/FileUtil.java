83e194d857007e4ed18f6d74fb40154fd2c334d5
Dmitry Rakovets <dmitryrakovets@gmail.com>
2021-12-03 06:55:58 +0300

    refactor: all structure for project

---
+++ apache-maven/intro/src/main/java/by/rakovets/course/java/tools/apache/maven/intro/example/FileUtil.java
@@ -0,0 +1,16 @@
+package by.rakovets.course.java.tools.apache.maven.intro.example;
+
+import java.util.Arrays;
+
+public class FileUtil {
+    public String getFileExtension(String filename) {
+        String[] split = filename.split("\\.");
+        return (split.length == 1) ? "" : split[split.length - 1];
+    }
+
+    // This method is not tested, so it may have errors
+    public String getFileName(String filename) {
+        String[] split = filename.split("\\.");
+        return (split.length == 1) ? "" : String.join("", Arrays.copyOfRange(split, 0, split.length - 1));
+    }
+}
---
+++ apache-maven/intro/src/main/java/by/rakovets/course/java/tools/apache/maven/intro/solution/Calculator.java
@@ -0,0 +1,19 @@
+package by.rakovets.course.java.tools.apache.maven.intro.solution;
+
+public class Calculator {
+    public double add(double firstNumber, double secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public double subtract(double firstNumber, double secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public double multiply(double firstNumber, double secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public double divide(double firstNumber, double secondNumber) {
+        return firstNumber / secondNumber;
+    }
+}
