5a1d4f52d359dbd2a65aac141b616264c4d32d74
Dmitry Rakovets <dmitryrakovets@gmail.com>
2022-04-12 06:36:06 +0300

    feat: add theme 'Validation'

---
+++ validation/src/main/java/by/rakovets/course/java/tools/validation/Example1.java
@@ -0,0 +1,27 @@
+package by.rakovets.course.java.tools.validation;
+
+import by.rakovets.course.java.tools.validation.model.User;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+import java.util.Set;
+
+public class Example1 {
+    public static void main(String[] args) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        User user = new User();
+        user.setWorking(true);
+        user.setAboutMe("Its all about me!");
+        user.setAge(50);
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+
+        for (ConstraintViolation<User> violation : violations) {
+            System.out.println(violation.getMessage());
+        }
+    }
+}
